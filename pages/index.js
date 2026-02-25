932ce8b95f85f2dfdd3a07010399b32f26e60987
HenriqueRocha0 <henrique.rocha.0505@gmail.com>
2026-02-25 23:22:21 +0000

    Modificação

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1> Olá Querida, eu te amo muito!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("Indentação errrada");
-}
-
 export default Home;
