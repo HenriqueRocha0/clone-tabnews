cf0d6b2db539dd031f1dd5259a0028cb75c4bf91
HenriqueRocha0 <henrique.rocha.0505@gmail.com>
2026-02-28 18:27:10 +0000

    finish `/api/v1/status`endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("GET to /api/v1/status verification", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  // return 200
   expect(response.status).toBe(200);
+  // updated time defined
+  const responseBody = await response.json();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  //version database defined
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
