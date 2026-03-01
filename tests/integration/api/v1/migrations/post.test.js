aac227efc1e9a29896ff79cf585dbc1bc43c5d6d
HenriqueRocha0 <henrique.rocha.0505@gmail.com>
2026-03-01 23:31:24 +0000

    adds `/api/v1/migration` endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,29 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(201);
+
+  const response1Boby = await response1.json();
+
+  expect(Array.isArray(response1Boby)).toBe(true);
+  expect(response1Boby.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Boby = await response2.json();
+
+  expect(Array.isArray(response2Boby)).toBe(true);
+  expect(response2Boby.length).toBe(0);
+});
