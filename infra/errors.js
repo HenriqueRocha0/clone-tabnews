336ebeaf400c5a5d960b36a2c125981cf1e9cd62
HenriqueRocha0 <henrique.rocha.0505@gmail.com>
2026-04-11 15:52:46 +0000

    feat: create and use `ValidationError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -11,7 +11,7 @@ export class InternalServerError extends Error {
   toJSON() {
     return {
       name: this.name,
-      Message: this.message,
+      message: this.message,
       action: this.action,
       status_code: this.statusCode,
     };
@@ -31,7 +31,26 @@ export class ServiceError extends Error {
   toJSON() {
     return {
       name: this.name,
-      Message: this.message,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
       action: this.action,
       status_code: this.statusCode,
     };
